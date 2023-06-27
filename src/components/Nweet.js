60aa4b1aa9047cd77ccea38601342ffd8a442115
naseokju <lukas95@naver.com>
2023-06-27 22:36:10 +0900

    Deleting Files

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,6 +1,7 @@
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
-import { dbService } from "../fbase";
+import { dbService, storageService } from "../fbase";
+import { deleteObject, ref } from "firebase/storage";
 
 const Nweet = ({ nweetObj, isOwner }) => {
     const [editing, setEditing] = useState(false);
@@ -10,6 +11,7 @@ const Nweet = ({ nweetObj, isOwner }) => {
         const ok = window.confirm("Are you sure you want to delete this nweet?");
         if(ok) {
             await deleteDoc(NweetTextRef);
+            await deleteObject(ref(storageService, nweetObj.attachmentUrl));
         }
     };
     const toggleEditing = () => setEditing((prev) => !prev);
