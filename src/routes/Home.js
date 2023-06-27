fc338123ad9f28560e9abd3caa16ddb6c5f8cc80
naseokju <lukas95@naver.com>
2023-06-27 21:09:56 +0900

    Getting the Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,24 @@
-import { addDoc, collection } from "firebase/firestore";
-import React, { useState } from "react";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../fbase";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const q = query(collection(dbService, "nweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
     const onSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -39,6 +54,13 @@ const Home = () => {
                     value="Nweet"
                 />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
