20b949efde8d74f801f878463c0f199e84e94dff
naseokju <lukas95@naver.com>
2023-06-27 16:30:47 +0900

    React + Firebase Setup

---
+++ src/fbase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeiA6n0G47_1h0PObvampahbo_pJIYZBw",
+  authDomain: "nwitter-d9531.firebaseapp.com",
+  projectId: "nwitter-d9531",
+  storageBucket: "nwitter-d9531.appspot.com",
+  messagingSenderId: "291837406441",
+  appId: "1:291837406441:web:6733c962020767c945d478"
+};
+
+const app = initializeApp(firebaseConfig);
