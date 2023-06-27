4c5e9b06a76522631175fd4bd327b4d344f721e3
naseokju <lukas95@naver.com>
2023-06-27 16:45:10 +0900

    Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,22 +1,43 @@
 import React, { useState } from "react";
+import { authService } from "../fbase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const onChange = (event) => {
-        const {target: {name, value}} = event;
+    const [newAccount, setNewAccount] = useState(true);
+    const onChange = (e) => {
+        const {
+            target: { name, value },
+        } = e;
+
         if(name === "email") {
             setEmail(value);
         } else if (name === "password") {
             setPassword(value);
         }
     }
-    const onSubmit = (event) => {
-        event.preventdefault();
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                // create account
+                data = await createUserWithEmailAndPassword(authService, email, password);
+            } else {
+                // login
+                data = await signInWithEmailAndPassword(authService, email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
     };
+    
     return (
-        <div>
-            <form onSubmit={onSubmit}>
+        <div onSubmit={onSubmit}>
+            <form>
                 <input 
                     name="email" 
                     type="text" 
@@ -33,7 +54,7 @@ const Auth = () => {
                     required
                     onChange={onChange}
                 />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
                 <button>Continue with Google</button>
