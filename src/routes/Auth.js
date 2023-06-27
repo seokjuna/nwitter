d7febd7be6905016cca7c41fa8e4d9b1176ad92b
naseokju <lukas95@naver.com>
2023-06-27 17:40:33 +0900

    Social Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { authService } from "../fbase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
@@ -18,12 +18,11 @@ const Auth = () => {
             setPassword(value);
         } 
     }
-
     const onSubmit = async (e) => {
         e.preventDefault();
         try {
             let data;
-            if (newAccount) {
+            if(newAccount) {
                 // create account
                 data = await createUserWithEmailAndPassword(authService, email, password);
             } else {
@@ -37,7 +36,20 @@ const Auth = () => {
     };
     const toggleAccount = () => {
         setNewAccount((prev) => !prev);
-    }
+    };
+    const onSocialClick = async (e) => {
+        const {
+            target: { name }
+        } = e;
+        let provider;
+        if(name === "google") {
+            provider = new GoogleAuthProvider();
+        } else if(name === "github") {
+            provider = new GithubAuthProvider();
+        }
+        const data = await signInWithPopup(authService, provider);
+        console.log(data);
+    };
     
     return (
         <div onSubmit={onSubmit}>
@@ -63,8 +75,8 @@ const Auth = () => {
             </form>
             <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div>
-                <button>Continue with Google</button>
-                <button>Contiune with Github</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Contiune with Github</button>
             </div>
         </div>
     );
